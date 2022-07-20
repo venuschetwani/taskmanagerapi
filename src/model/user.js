78c27a3472083aa45a0292a42ef23d7f4bad8197
venuschetwani <venuschetwani@gmail.com>
2022-07-20 11:24:59 +0530

    changes

--- src/model/user.js
+++ src/model/user.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
-const jwt =require('jsonwebtoken')
-const task=require('../model/task')
-require("dotenv").config({path: '../config/.env'});
+const jwt = require('jsonwebtoken')
+const task = require('../model/task')
+require("dotenv").config({ path: '../config/.env' });
+const datenow = new Date()
 const userSchema = new mongoose.Schema({
   id: {
     type: Number,
@@ -13,14 +14,11 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
+  
   name: {
     type: String,
     trim: true,
     required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
   },
   email: {
     type: String,
@@ -44,94 +42,145 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
-  tokens:[{
-    token:String,
-    //required:true
+  lastLogin: {
+    type: String,
+    default: datenow.toLocaleString().replace(',', ' ')
+  },
+  tokens: [{
+    token: String,
+
   }],
-  nature:{
-    type:Buffer
+  nature: {
+    type: Buffer
   }
-},{
-  timestamps:true
+}, {
+  timestamps: true
 });
 
 
 //relation 
-userSchema.virtual('user_task',{
-  ref:'Task',
-  localField:'_id',
-  foreignField:'owner'
+userSchema.virtual('user_task', {
+  ref: 'Task',
+  localField: '_id',
+  foreignField: 'owner'
 })
 
 //plain text to hash password before saving the user
 userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+
+//login confirmation
+userSchema.statics.findByCredintials = async (email, password) => {
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("no such user");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("unauthorized");
+  }
+
+  return user
+};
+
+// //lastlogin
+userSchema.statics.lastLogin = async (id) => {
+  try {
+    
     const user = this;
-    if (user.isModified("password")) {
-      user.password = await bcrypt.hash(user.password, 8);
-    }
-    next();
-  });
-  
+    const DateNow = new Date()
+    const d = DateNow.toLocaleString().replace(',', ' ')
+    user=await User.findByIdAndUpdate( id, { lastLogin: d },
+      function (err, docs) {
+        if (!err) {
+          //console.log(docs);
+          return user;
+        } else {
+          //console.error(err);
+        }
+      })
+   
+  return user
+  } catch (error) {
+   //console.error(error);
+  }
+}
 
-  //login confirmation
-  userSchema.statics.findByCredintials = async (email, password) => {
-  
-    const user = await User.findOne({ email });
-    if (!user) {
-      throw new Error("unable to login");
-    }
-  
-    const isMatch = await bcrypt.compare(password, user.password);
-  
-    if (!isMatch) {
-      throw new Error("unable to login");
-    }
-    console.log(user);
-       return user
-  };
 
-  //token generate
 
-  userSchema.methods.tokenauthkey=async function(){
-    const user=this
-    const token=jwt.sign({_id:user._id},process.env.JWT_SECRETKEY,{expiresIn: '24h'})
+
+//token generate
+userSchema.methods.tokenauthkey = async function () {
+  const user = this
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRETKEY, { expiresIn: '5 seconds' })
   //console.log(token);
-    user.tokens=user.tokens.concat({token})
-    await user.save()
-    return token
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+  return token
+}
+
+//checking the expiry token
+userSchema.statics.checkTokenExpiry=(token)=>{
+  var isExpiredToken=false
+
+  var decoded = jwt.decode(token);
+  console.log(decoded);
+    
+    var dateNow = new Date();
+    
+  if (decoded.exp*1000 > dateNow.getTime()) {
+    isExpiredToken = false;
+    console.log("not expired")
+    
   }
+  else{
+  isExpiredToken = true;
+  console.log("is expired");
+ }
+ return isExpiredToken
+ }
 
 
 
 
-  //hiding the data
-  userSchema.methods.toJSON=function(){
-       const user=this
-       const objectUser=user.toObject()
 
-       delete objectUser.password
-       delete objectUser.tokens
-       delete objectUser.nature
+//hiding the data
+userSchema.methods.toJSON = function () {
+  const user = this
+  const objectUser = user.toObject()
 
-       return objectUser
+  delete objectUser.password
+  delete objectUser.tokens
+  delete objectUser.nature
 
-  }
-  
+  return objectUser
 
-  //delete task when user is deleted
-  userSchema.pre('remove',async function(next){
-    const user=this;
-    await task.deleteMany({owner:user._id})
+}
 
-    next()
-  })
-  
 
-  const User = new mongoose.model("User", userSchema);
-  
-  module.exports =User
+//delete task when user is deleted
+userSchema.pre('remove', async function (next) {
+  const user = this;
+  await task.deleteMany({ owner: user._id })
 
-  
+  next()
+})
+
+
+const User = new mongoose.model("User", userSchema);
+
+module.exports = User
+
+
 
 
 
--- src/routers/login.js
+++ src/routers/login.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const User = require("../model/user");
 require("../db.js/moongoose");
-const jwt =require('jsonwebtoken')
-const multer=require('multer')
-const auth=require('../middleware/auth')
-const sharp=require('sharp')
-const {sendWelcomemail}=require("../emails/account")
+const jwt = require('jsonwebtoken')
+const multer = require('multer')
+const auth = require('../middleware/auth')
+
+const sharp = require('sharp')
+const { sendWelcomemail } = require("../emails/account")
 
 const app = express();
 
@@ -14,16 +15,16 @@ app.use(express.json());
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
-try{
-     await user.save()
-     sendWelcomemail(user.email,user.name)
-     
-      res.status(201).send(user)
-    }
-catch(e) {
-      res.status(404).send();
-    }
-  
+  try {
+    await user.save()
+    sendWelcomemail(user.email, user.name)
+
+    res.status(201).send(user)
+  }
+  catch (e) {
+    res.status(404).send();
+  }
+
 })
 
 router.post("/users/login", async (req, res) => {
@@ -32,9 +33,19 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     )
-    user.findByIdAndUpdate(user._id, {lastLogin: Date.now().toISOString()})
+
+    await User.lastLogin(user._id)
+     //console.log(user);
     const token = await user.tokenauthkey()
-    res.send({user,token})
+    const isExpired = await User.checkTokenExpiry(token)
+     console.log(isExpired);
+    if (isExpired){
+      res.status(404).send("token is expired")
+    }
+
+  
+    else{
+    res.send({ user, token })}
   } catch (e) {
     res.status(500).send(e);
   }
@@ -42,59 +53,57 @@ router.post("/users/login", async (req, res) => {
 
 module.exports = router;
 
+//image uploading
 
+const nature = multer({
 
-const nature=multer({
-  
-  limits:{
-    fileSize:1000000
+  limits: {
+    fileSize: 1000000
   },
-  fileFilter(req,file,cb){
-    
+  fileFilter(req, file, cb) {
 
-    if(!file.originalname.match(/(\.jpg|jpeg|png)$/))
-    {
+
+    if (!file.originalname.match(/(\.jpg|jpeg|png)$/)) {
       return cb(new Error('please upload word file'))
     }
-    cb(undefined,true)
+    cb(undefined, true)
   }
 })
 
-router.post('/nature' ,auth,nature.single( 'nature'),async(req,res)=>{
-  const buffer= await sharp(req.file.buffer).resize({width:50,height:50}).png().toBuffer()
-  req.user.nature=buffer
+router.post('/nature', auth, nature.single('nature'), async (req, res) => {
+  const buffer = await sharp(req.file.buffer).resize({ width: 50, height: 50 }).png().toBuffer()
+  req.user.nature = buffer
   await req.user.save()
   res.send('binary file saved')
-},(error,req,res,next)=>{
+}, (error, req, res, next) => {
   res.status(400).send({
     error: error.message
   })
 }
 )
 
-router.delete('/nature',auth,async(req,res)=>{
-  req.user.nature=undefined
+router.delete('/nature', auth, async (req, res) => {
+  req.user.nature = undefined
   await req.user.save()
   res.send()
-},(error,req,res,next)=>{
+}, (error, req, res, next) => {
   res.status(400).send({
     error: error.message
-  })})
-
-  router.get('/:id/nature',async(req,res)=>{
-    try{
-     const user=await User.findById(req.params.id)
-
-     if(!user || !user.nature)
-     {
-       throw new Error()
-     }
-     res.set('Content-Type','image/png')
-     res.send(user.nature)
-
-
-    }catch(e)
-    {
-      res.status(404).send()
-    }
   })
+})
+
+router.get('/:id/nature', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+
+    if (!user || !user.nature) {
+      throw new Error()
+    }
+    res.set('Content-Type', 'image/png')
+    res.send(user.nature)
+
+
+  } catch (e) {
+    res.status(404).send()
+  }
+})
