fd07601b37cc7de0ee83143732ffac310ff2cca3
venuschetwani <venuschetwani@gmail.com>
2022-07-27 18:43:21 +0530

    init

--- src/app.js
+++ src/app.js
@@ -17,16 +17,9 @@ app.use(task_router);
 app.use(auth_router);
 
 app.listen(port, () => console.log(`express on ${port}`));
-// console.log(process.env.APP_HOST);
-// mongoose
-//     .connect(process.env.URL, {
-//         useNewUrlParser: true,
-//     })
-//     .then(() => {
-//         console.log("connected");
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
+
+app.get("/",(req,res)=>{
+    res.json('Hello')
+})
 
 module.exports = app
